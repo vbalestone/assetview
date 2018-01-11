613652c499287bd0537f1f6448564576ae8f9f3f
vbalestone <vilsonbalestero@gmail.com>
2018-01-11 08:30:46 -0200

    Demo project using SpringFramework 5.

---
+++ src/main/java/com/smar/assetview/AssetviewApplication.java
@@ -0,0 +1,12 @@
+package com.smar.assetview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetviewApplication.class, args);
+	}
+}
---
+++ src/test/java/com/smar/assetview/AssetviewApplicationTests.java
@@ -0,0 +1,16 @@
+package com.smar.assetview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AssetviewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
